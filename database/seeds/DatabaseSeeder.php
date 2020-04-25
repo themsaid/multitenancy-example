1c3b2e3cb35ea4a9a9fa63e1daf6d83088d5df2a
Mohamed Said <themohamedsaid@gmail.com>
2020-04-25 15:15:43 +0200

    wip

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,14 +7,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      *
-     * @return void
      */
     public function run()
     {
         if ($_SERVER['argv'][1] != 'tenants:migrate') {
-            $this->call(LandlordSeeder::class);
-        } else {
-            $this->call(UserSeeder::class);
+            return $this->call(LandlordSeeder::class);
         }
+
+        $this->call(UserSeeder::class);
     }
 }
